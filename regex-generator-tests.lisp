f0200b4d78b43639da645e4c21bbad085d2cbf3d
cxxxr <g23tlm@gmail.com>
2020-06-04 19:35:14 +0900

    fix char-class and tests

--- regex-generator-tests.lisp
+++ regex-generator-tests.lisp
@@ -2,12 +2,17 @@
   (:use :cl :regex-generator :rove :alexandria))
 (in-package #:regex-generator-tests)
 
+(defun ensure-char (string)
+  (assert (and (stringp string)
+               (= 1 (length string))))
+  (char string 0))
+
 (defun simple-test (regex test expected-num)
   (loop :with table := (make-array 128 :initial-element nil)
         :repeat 1000
-        :for c := (generate regex)
-        :always (funcall test c)
-        :do (setf (aref table (char-code c)) t)
+        :for string := (generate regex)
+        :always (and (stringp string) (= 1 (length string)) (funcall test (char string 0)))
+        :do (setf (aref table (char-code (char string 0))) t)
         :finally (return (= expected-num (count t table)))))
 
 (deftest simple-regex-tests
@@ -27,29 +32,29 @@
   (ok (string= "ab" (generate '(:sequence "a" "b"))))
   (ok (loop :with chars := '()
             :repeat 100
-            :for c := (generate "a|b")
+            :for c := (ensure-char (generate "a|b"))
             :do (pushnew c chars)
             :finally (return (length= chars 2))))
   (ok (loop :with chars := '()
             :repeat 100
-            :for c := (generate "a|b|c")
+            :for c := (ensure-char (generate "a|b|c"))
             :do (pushnew c chars)
             :finally (return (length= chars 3))))
   (ok (string= "ab" (generate "(?:ab)")))
   (ok (string= "abcd" (generate '(:group "ab" "cd"))))
   (ok (loop :with chars := '()
             :repeat 100
-            :for c := (generate "[a-c]")
+            :for c := (ensure-char (generate "[a-c]"))
             :do (pushnew c chars)
             :finally (return (length= chars 3))))
   (ok (loop :with chars := '()
             :repeat 100
-            :for c := (generate "[a-c0-2_]")
+            :for c := (ensure-char (generate "[a-c0-2_]"))
             :do (pushnew c chars)
             :finally (return (length= chars 7))))
   (ok (loop :with chars := '()
             :repeat 100
-            :for c := (generate "[abc]")
+            :for c := (ensure-char (generate "[abc]"))
             :do (pushnew c chars)
             :finally (return (length= chars 3))))
   (ok (loop :repeat 10 :always (ppcre:scan "^a*$" (generate "a*"))))
